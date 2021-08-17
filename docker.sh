fe74c87737165ff48039a6b414433bad666483b7
unknownpgr <unknownpgr@gmail.com>
2021-08-17 05:02:12 +0000

    Add ssh directory mount

--- docker.sh
+++ docker.sh
@@ -1,6 +1,9 @@
-docker run      \
---rm -it        \
--v $(pwd):/app  \
--w /app         \
-node:14         \
+# Docker execute script for development
+
+docker run                             \
+--rm -it                               \
+-v $(pwd):/app                         \
+-v $(pwd)/deploy-key:/root/.ssh        \
+-w /app                                \
+node:14                                \
 /bin/bash
